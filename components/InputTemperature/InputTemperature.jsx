fc2aa8483309642470d01ad1f4ab5edcf7ae0c11
JulienGrade <gradejulien@gmail.com>
2024-03-23 12:26:42 +0100

    Add TemperatureDisplay

--- components/InputTemperature/InputTemperature.jsx
+++ components/InputTemperature/InputTemperature.jsx
@@ -1,15 +1,16 @@
 import { Text, TextInput, View } from "react-native";
 import { s } from "./InputTemperature.style.js";
 
-export function InputTemperature({ defaultValue }) {
+export function InputTemperature({ defaultValue, onChangeText }) {
     return (
         <View style={s.container}>
             <TextInput
                 style={s.input}
-                placeholder="Tappe une température"
+                placeholder="Saisis une température"
                 keyboardType="numeric"
                 maxLength={4}
                 defaultValue={defaultValue}
+                onChangeText={onChangeText}
             />
             <Text style={s.unit}>°C</Text>
         </View>
